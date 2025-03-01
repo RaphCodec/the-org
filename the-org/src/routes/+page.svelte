4e96faa555ac5804b915baab50f0726d99bcfab9
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-03-01 10:04:16 -0500

    Began new svelte flowbite version of the-org

---
+++ the-org/src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	// import Counter from './Counter.svelte';
+	// import welcome from '$lib/images/svelte-welcome.webp';
+	// import welcomeFallback from '$lib/images/svelte-welcome.png';
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, SidebarDropdownItem, SidebarDropdownWrapper } from 'flowbite-svelte';
+	import { ChartPieSolid, CartSolid, ChevronDoubleUpOutline, ChevronDoubleDownOutline } from 'flowbite-svelte-icons';
+	let spanClass = 'flex-1 ms-3 whitespace-nowrap';
+</script>
+
+<svelte:head>
+	<title>The Org</title>
+	<meta name="description" content="The Org - Organization Management Application" />
+</svelte:head>
+
+<Sidebar>
+	<SidebarWrapper>
+	  <SidebarGroup>
+		<SidebarItem label="Dashboard">
+		  <svelte:fragment slot="icon">
+			<ChartPieSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+		  </svelte:fragment>
+		</SidebarItem>
+		<SidebarDropdownWrapper label="E-commerce">
+		  <svelte:fragment slot="icon">
+			<CartSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+		  </svelte:fragment>
+		  <svelte:fragment slot="arrowup">
+			<ChevronDoubleUpOutline class="w-6 h-6" />
+		  </svelte:fragment>
+		  <svelte:fragment slot="arrowdown">
+			<ChevronDoubleDownOutline class="w-6 h-6" />
+		  </svelte:fragment>
+		  <SidebarDropdownItem label="Products" />
+		  <SidebarDropdownItem label="Billing" />
+		  <SidebarDropdownItem label="Invoice" />
+		</SidebarDropdownWrapper>
+	  </SidebarGroup>
+	</SidebarWrapper>
+  </Sidebar>
+
+
+
---
+++ the-org/src/routes/Header.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
+	import { SearchOutline } from 'flowbite-svelte-icons';
+</script>
+
+<header>
+	<Navbar>
+		<NavBrand href="/">
+		  <img src="{logo}" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+		  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">RC</span>
+		</NavBrand>
+		<div>
+			The Org
+		</div>
+		<div class="flex md:order-2">
+		  <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-hidden focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+			<SearchOutline class="w-5 h-5" />
+		  </Button>
+		  <div class="hidden relative md:block">
+			<div class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none">
+			  <SearchOutline class="w-4 h-4" />
+			</div>
+			<Input id="search-navbar" class="ps-10" placeholder="Search..." />
+		  </div>
+		  <NavHamburger />
+		</div>
+	  </Navbar>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
