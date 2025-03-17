ba26bc5527b4e4a2d151f5731ca673025b00445b
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-03-16 20:37:02 -0400

    Switched to React and started new page based off of a joy ui template

---
+++ the-org-frontend/src/components/Header.tsx
@@ -0,0 +1,223 @@
+import * as React from 'react';
+import { useColorScheme } from '@mui/joy/styles';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import IconButton from '@mui/joy/IconButton';
+import Stack from '@mui/joy/Stack';
+import Avatar from '@mui/joy/Avatar';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+import Tooltip from '@mui/joy/Tooltip';
+import Dropdown from '@mui/joy/Dropdown';
+import Menu from '@mui/joy/Menu';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+import ListDivider from '@mui/joy/ListDivider';
+import Drawer from '@mui/joy/Drawer';
+import ModalClose from '@mui/joy/ModalClose';
+import DialogTitle from '@mui/joy/DialogTitle';
+
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import BookRoundedIcon from '@mui/icons-material/BookRounded';
+import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import GroupIcon from '@mui/icons-material/Group';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import PersonIcon from '@mui/icons-material/Person';
+
+import Navigation from './Navigation';
+import { GitHub, Person } from '@mui/icons-material';
+
+function ColorSchemeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <IconButton size="sm" variant="outlined" color="primary" />;
+  }
+  return (
+    <Tooltip title="Change theme" variant="outlined">
+      <IconButton
+        data-screenshot="toggle-mode"
+        size="sm"
+        variant="plain"
+        color="neutral"
+        sx={{ alignSelf: 'center' }}
+        onClick={() => {
+          if (mode === 'light') {
+            setMode('dark');
+          } else {
+            setMode('light');
+          }
+        }}
+      >
+        {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+export default function Header() {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between' }}>
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          display: { xs: 'none', sm: 'flex' },
+        }}
+      >
+        <IconButton
+          size="md"
+          variant="outlined"
+          color="neutral"
+          sx={{ display: { xs: 'none', sm: 'inline-flex' }, borderRadius: '50%' }}
+        >
+          <GroupIcon />
+        </IconButton>
+        <Typography level="title-md" textColor="text.primary">
+          The Org
+        </Typography>
+        {/* <Button
+          variant="plain"
+          color="neutral"
+          component="a"
+          href="/joy-ui/getting-started/templates/email/"
+          size="sm"
+          sx={{ alignSelf: 'center' }}
+        >
+          Email
+        </Button> */}
+      </Stack>
+      <Box sx={{ display: { xs: 'inline-flex', sm: 'none' } }}>
+        <IconButton variant="plain" color="neutral" onClick={() => setOpen(true)}>
+          <MenuRoundedIcon />
+        </IconButton>
+        <Drawer
+          sx={{ display: { xs: 'inline-flex', sm: 'none' } }}
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <ModalClose />
+          <DialogTitle>Acme Co.</DialogTitle>
+          <Box sx={{ px: 1 }}>
+            <Navigation />
+          </Box>
+        </Drawer>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 1.5,
+          alignItems: 'center',
+        }}
+      >
+        <Input
+          size="sm"
+          variant="outlined"
+          placeholder="Search anything…"
+          startDecorator={<SearchRoundedIcon color="primary" />}
+          endDecorator={
+            <IconButton
+              variant="outlined"
+              color="neutral"
+              sx={{ bgcolor: 'background.level1' }}
+            >
+              <Typography level="title-sm" textColor="text.icon">
+                ⌘ K
+              </Typography>
+            </IconButton>
+          }
+          sx={{
+            alignSelf: 'center',
+            display: {
+              xs: 'none',
+              sm: 'flex',
+            },
+          }}
+        />
+        <IconButton
+          size="sm"
+          variant="outlined"
+          color="neutral"
+          sx={{ display: { xs: 'inline-flex', sm: 'none' }, alignSelf: 'center' }}
+        >
+          <SearchRoundedIcon />
+        </IconButton>
+        <Tooltip title="Source Code" variant="outlined">
+          <IconButton
+            size="sm"
+            variant="plain"
+            color="neutral"
+            component="a"
+            href="https://github.com/RaphCodec/the-org"
+            sx={{ alignSelf: 'center' }}
+          >
+            <GitHub />
+          </IconButton>
+        </Tooltip>
+        <ColorSchemeToggle />
+        <Dropdown>
+          <MenuButton
+            variant="plain"
+            size="sm"
+            sx={{ maxWidth: '32px', maxHeight: '32px', borderRadius: '9999999px' }}
+          >
+          <PersonIcon />
+          </MenuButton>
+          <Menu
+            placement="bottom-end"
+            size="sm"
+            sx={{
+              zIndex: '99999',
+              p: 1,
+              gap: 1,
+              '--ListItem-radius': 'var(--joy-radius-sm)',
+            }}
+          >
+            <MenuItem>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <PersonIcon />
+                <Box sx={{ ml: 1.5 }}>
+                  <Typography level="title-sm" textColor="text.primary">
+                    Raphael Clifton
+                  </Typography>
+                  <Typography level="body-xs" textColor="text.tertiary">
+                  https://github.com/RaphCodec
+                  </Typography>
+                </Box>
+              </Box>
+            </MenuItem>
+            <ListDivider />
+            <MenuItem>
+              <HelpRoundedIcon />
+              Help
+            </MenuItem>
+            <MenuItem>
+              <SettingsRoundedIcon />
+              Settings
+            </MenuItem>
+            <ListDivider />
+            <MenuItem
+              component="a"
+              href="https://github.com/RaphCodec"
+            >
+              Github
+              <OpenInNewRoundedIcon />
+            </MenuItem>
+          </Menu>
+        </Dropdown>
+      </Box>
+    </Box>
+  );
+}
---
+++ the-org-frontend/src/components/Navigation.tsx
@@ -0,0 +1,190 @@
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import List from '@mui/joy/List';
+import ListSubheader from '@mui/joy/ListSubheader';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import ListItemContent from '@mui/joy/ListItemContent';
+
+import FolderRoundedIcon from '@mui/icons-material/FolderRounded';
+import ShareRoundedIcon from '@mui/icons-material/ShareRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import AccountTreeRoundedIcon from '@mui/icons-material/AccountTreeRounded';
+import FitScreenIcon from '@mui/icons-material/FitScreen';
+import CropRotateIcon from '@mui/icons-material/CropRotate';
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import ZoomOutIcon from '@mui/icons-material/ZoomOut';
+import EditIcon from '@mui/icons-material/Edit';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import MediationIcon from '@mui/icons-material/Mediation';
+import UnpublishedIcon from '@mui/icons-material/Unpublished';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import ImageIcon from '@mui/icons-material/Image';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+
+export default function Navigation() {
+  return (
+    <List size="sm" sx={{ '--ListItem-radius': '8px', '--List-gap': '2px' }}>
+      <ListItem nested>
+        <ListSubheader sx={{ letterSpacing: '2px', fontWeight: '800' }}>
+          Chart View
+        </ListSubheader>
+        <List
+          aria-labelledby="nav-list-browse"
+          sx={{ '& .JoyListItemButton-root': { p: '8px' } }}
+        >
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <FitScreenIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Fit</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <CropRotateIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Rotate</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <AccountTreeRoundedIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Compact</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <ZoomInIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Zoom In</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <ZoomOutIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Zoom Out</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+        </List>
+      </ListItem>
+
+      <ListItem nested sx={{ mt: 2 }}>
+        <ListSubheader sx={{ letterSpacing: '2px', fontWeight: '800' }}>
+          Node Actions
+        </ListSubheader>
+
+        <List
+          aria-labelledby="nav-list-tags"
+          size="sm"
+          sx={{
+            '--ListItemDecorator-size': '32px',
+            '& .JoyListItemButton-root': { p: '8px' },
+          }}
+        >
+
+<ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <EditIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Edit</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <PersonAddIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Add</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <PersonRemoveIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Remove</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <MediationIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Show Lineage</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <UnpublishedIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Deselect All</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListSubheader sx={{ letterSpacing: '2px', fontWeight: '800' }}>
+          Exports
+        </ListSubheader>
+
+        <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <ImageIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>PNG</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+        <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <PhotoLibraryIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>SVG</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+        <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <PictureAsPdfIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>PDF</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+        <ListItem>
+            <ListItemButton>
+              <ListItemDecorator>
+                <DataObjectIcon fontSize="small" />
+              </ListItemDecorator>
+              <ListItemContent>Data</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+        </List>
+      </ListItem>
+    </List>
+  );
+}
