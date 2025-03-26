98ca7ca39647036bba4a7086459bbfe420b958bd
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-03-25 20:05:05 -0400

    deleted old svelte files

--- the-org/src/lib/components/FunctionAlerts.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { Alert } from 'flowbite-svelte';
-  export let message = 'Default alert!';
-  export let color = 'red';
-</script>
-
-<Alert color={color}> 
-  <span class="font-medium">{message}</span>
-</Alert>
