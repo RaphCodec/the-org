150618059c4f5a391316eebbebde0f4f4685a4a6
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-04-06 14:03:42 -0400

    Fastapi switch (#6)
    * Switched to FastAPI + DaisyUI + Vanilla JS

---
+++ static/js/chart_view.js
@@ -0,0 +1,38 @@
+function fitChart() {
+    chart.fit();
+}
+function rotateChart() {
+	chart
+		.layout(['right', 'bottom', 'left', 'top'][index++ % 4])
+		.render()
+		.fit();
+}
+
+function compactChart() {
+	chart
+		.compact(!!(compact++ % 2))
+		.render()
+		.fit();
+}
+
+function zoomInChart() {
+	chart.zoomIn();
+}
+
+function zoomOutChart() {
+	chart.zoomOut();
+}
+
+function filterChart(e) {
+	const value = e.srcElement.value;
+	chart.clearHighlighting();
+	const data = chart.data();
+	data.forEach((d) => (d._expanded = false));
+	data.forEach((d) => {
+		if (value != '' && d.name.toLowerCase().includes(value.toLowerCase())) {
+			d._highlighted = true;
+			d._expanded = true;
+		}
+	});
+	chart.data(data).render().fit();
+}
