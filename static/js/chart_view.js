5aab9c5dc0cc9489f2e8c32c8084012f43c82757
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-04-13 16:18:34 -0400

    added levels list to display entire chart depth

--- static/js/chart_view.js
+++ static/js/chart_view.js
@@ -1,5 +1,5 @@
 function fitChart() {
-    chart.fit();
+	chart.fit();
 }
 function rotateChart() {
 	chart
@@ -39,4 +39,41 @@ function toggleReports() {
 	totalReports.forEach((report) => {
 		report.classList.toggle('hidden');
 	});
+}
+
+function getLevels() {
+	const attrs = chart.getChartState();
+	const root = attrs.generateRoot(attrs.data);
+	const levels = new Set(root.descendants().map(node => node.depth));
+	return levels
+}
+
+function showLevels() {
+	const levels = getLevels();
+	const levelsList = document.getElementById('levelsList');
+
+	levelsList.innerHTML = `
+		<li class="p-4 pb-2 text-xs opacity-60 tracking-wide text-center">Pick a level to expand</li>
+	`;
+
+	levels.forEach(level => {
+		const listItem = document.createElement('li');
+		listItem.className = 'list-row flex flex-col items-center gap-2';
+
+		const link = document.createElement('span');
+		link.className = 'text-4xl font-thin opacity-30 tabular-nums hover:opacity-100 hover:text-primary transition cursor-pointer';
+		link.textContent = level.toString().padStart(2, '0');
+		link.onclick = () => selectLevel(level);
+
+		listItem.appendChild(link);
+		levelsList.appendChild(listItem);
+	});
+
+	if (levelsList.classList.contains('hidden')) {
+		levelsList.classList.remove('hidden');
+	}
+}
+
+function selectLevel(level) {
+	console.log(`Level ${level} selected`);
 }
