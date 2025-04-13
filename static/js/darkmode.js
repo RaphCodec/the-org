e13b0d1616f7529d7bdb7c6c305e97385a117582
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-04-13 09:07:35 -0400

    Toggle attributes (#10)
    * added toggle salaries and reports
    
    * converted node content to tailwindcss
    
    * made salaries hidden by default
    
    * replaced id with level

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeSwitch = document.getElementById('theme-switch');
   themeSwitch.addEventListener('change', () => {
     if (themeSwitch.checked) {
-      document.documentElement.setAttribute('data-theme', 'winter'); // Dark mode
+      document.documentElement.setAttribute('data-theme', 'nord'); // Dark mode
     } else {
       document.documentElement.setAttribute('data-theme', 'dark'); // Light mode
     }
--- static/js/org.js
+++ static/js/org.js
@@ -26,63 +26,67 @@ fetch("/data")
       .siblingsMargin((d) => 100)
       .initialZoom(0.7)
       .nodeContent(function (d, i, arr, state) {
-        const color = "#FFFFFF";
-        const imageDiffVert = 25 + 2;
+        const imageDiffVert = 27;
         return `
-      <div style='width:${
-        d.width
-      }px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
-              <div style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: ${d.data._highlighted || d.data._upToTheRootHighlighted ? '5px solid #FA3C3C"' : '1px solid #E4E2E9"'} >
-                  <div style="color:#08011E;display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${
-                    d.data.id
-                  }</div>
-                  <div style="background-color:${color};margin-top:${-imageDiffVert - 20}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
-                  <div style="margin-top:${
-                    -imageDiffVert - 20
-                  }px;">   <img src=" ${d.data.image}" style="margin-left:${20}px;border-radius:100px;width:40px;height:40px;" /></div>
-                  <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:10px">  ${
-                    d.data.name
-                  } </div>
-                  <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
-                    d.data.position
-                  } </div>
-                  <div class="node-salaries" style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
-                    d.data.salary
-                  } </div>
-                   <div style="display:flex;justify-content:space-between;padding-left:15px;padding-right:15px;">
-                        <div style="color:#08011E; position: absolute; bottom: 10px; left: 15px;" class="node-direct-reports">Directs: ${
-                          d.data._directSubordinates
-                        } </div>  
-                        <div style="color:#08011E; position: absolute; bottom: 10px; right: 15px;" class="node-total-reports">Total: ${
-                          d.data._totalSubordinates
-                        } </div>
-                    </div>
-
+            <div class="w-[${
+              d.width
+            }px] h-[${d.height}px] pt-[${imageDiffVert - 2}px] px-[1px]">
+              <div class="font-inter bg-white -ml-[1px] w-[${
+                d.width - 2
+              }px] h-[${d.height - imageDiffVert}px] rounded-lg ${d.data._highlighted || d.data._upToTheRootHighlighted ? "border-[5px] border-[#FA3C3C]" : "border border-[#E4E2E9]"}">
+            <div class="text-[#08011E] flex justify-end mt-[5px] mr-[8px]">Level: ${
+              d.depth
+            }</div>
+            <div class="bg-white -mt-[${
+              imageDiffVert + 20
+            }px] ml-[15px] rounded-full w-[50px] h-[50px]"></div>
+            <div class="-mt-[${imageDiffVert + 20}px]">
+              <img src="${
+                d.data.image
+              }" class="ml-[20px] rounded-full w-[40px] h-[40px]" />
+            </div>
+            <div class="text-[15px] text-[#08011E] ml-[20px] mt-[10px]">${
+              d.data.name
+            }</div>
+            <div class="text-[#716E7B] ml-[20px] mt-[3px] text-[10px]">${
+              d.data.position
+            }</div>
+            <div class="node-salaries text-[#716E7B] ml-[20px] mt-[3px] text-[10px] hidden">${
+              d.data.salary
+            }</div>
+            <div class="flex justify-between px-[15px]">
+              <div class="node-direct-reports text-[#08011E] absolute bottom-[10px] left-[15px]">Directs: ${
+                d.data._directSubordinates
+              }</div>
+              <div class="node-total-reports text-[#08011E] absolute bottom-[10px] right-[15px]">Total: ${
+                d.data._totalSubordinates
+              }</div>
+            </div>
               </div>
           </div>
-                  `;
+        `;
       })
       .nodeUpdate(function (d) {
         // Needed to disable default highlight behavior
         d3.select(this).select(".node-rect").attr("stroke", "none");
         // Needed for drag and drop to work
-        d3.select(this).classed('droppable', true);
-        d3.select(this).classed('draggable', d.id !== '1');
+        d3.select(this).classed("droppable", true);
+        d3.select(this).classed("draggable", d.id !== "1");
       })
       .nodeEnter(function (node) {
         d3.select(this).call(
           d3
             .drag()
             .filter(function (x, node) {
-              return dragEnabled && this.classList.contains('draggable');
+              return dragEnabled && this.classList.contains("draggable");
             })
-            .on('start', function (d, node) {
+            .on("start", function (d, node) {
               onDragStart(this, d, node);
             })
-            .on('drag', function (dragEvent, node) {
+            .on("drag", function (dragEvent, node) {
               onDrag(this, dragEvent);
             })
-            .on('end', function (d) {
+            .on("end", function (d) {
               onDragEnd(this, d);
             })
         );
