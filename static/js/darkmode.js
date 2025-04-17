bf779272a2ef935398d193fade8092d687f1818a
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-04-17 13:06:25 -0400

    Display lineage (#13)
    * added lineage fucntions
    
    * added expand level to the get lineage function

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeSwitch = document.getElementById('theme-switch');
   themeSwitch.addEventListener('change', () => {
     if (themeSwitch.checked) {
-      document.documentElement.setAttribute('data-theme', 'cupcake'); // Dark mode
+      document.documentElement.setAttribute('data-theme', 'lofi'); 
     } else {
-      document.documentElement.setAttribute('data-theme', 'sunset'); // Light mode
+      document.documentElement.setAttribute('data-theme', 'dark'); 
     }
   });
 });
