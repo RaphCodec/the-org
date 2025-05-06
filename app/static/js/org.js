d6ebbc425120d9ee8de6fa1566578833c1dc358a
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-05-05 20:40:40 -0400

    refactor: redid how salaries are hidden and exported

--- app/static/js/org.js
+++ app/static/js/org.js
@@ -4,6 +4,53 @@ var actNdCent = 0;
 var chart; //needed for chart to be displayed
 
 var currentlySelected = []; //stores ID's of clicked nodes
+let salaryHidden = true; 
+
+function genContent(d) {
+  /*
+    Node content needs to be normal CSS for PNG and SVG 
+    exports to work.
+    DO NOT CHANGE NODE CONTENT TO TAILWIND CSS
+
+    Also, the data attibutes (d.data.id, etc.) need to 
+    match the column names in the models.py fiel exactly
+    and is case sensitive.
+  */
+  const color = "#FFFFFF";
+  const imageDiffVert = 25 + 2;
+  return `
+      <div style='width:${
+  d.width
+      }px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
+        <div style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: ${d.data._highlighted || d.data._upToTheRootHighlighted ? '5px solid #FA3C3C"' : '1px solid #E4E2E9"'} >
+      <div style="color:#08011E;display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${
+        d.data.id
+      }</div>
+      <div style="background-color:${color};margin-top:${-imageDiffVert - 20}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
+      <div style="margin-top:${
+        -imageDiffVert - 20
+      }px;">   <img src=" ${d.data.image}" style="margin-left:${20}px;border-radius:100px;width:40px;height:40px;" /></div>
+      <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:10px">  ${
+        d.data.name
+      } </div>
+      <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
+        d.data.position
+      } </div>
+      <div class="node-salaries" style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;display:${salaryHidden ? 'none' : 'block'};"> ${
+        new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(d.data.salary)
+      } </div>
+       <div style="display:flex;justify-content:space-between;padding-left:15px;padding-right:15px;">
+      <div style="color:#08011E; position: absolute; bottom: 10px; left: 15px;" class="node-direct-reports">Directs: ${
+        d.data._directSubordinates
+      } </div>  
+      <div style="color:#08011E; position: absolute; bottom: 10px; right: 15px;" class="node-total-reports">Total: ${
+        d.data._totalSubordinates
+      } </div>
+
+        </div>
+    </div>
+      `;
+}
 
 fetch("/data")
   .then((response) => {
@@ -25,52 +72,9 @@ fetch("/data")
       .neighbourMargin((a, b) => 20)
       .siblingsMargin((d) => 100)
       .initialZoom(0.7)
-      /*
-       Node content needs to be normal CSS for PNG and SVG 
-       exports to work.
-       DO NOT CHANGE NODE CONTENT TO TAILWIND CSS
-
-       Also, the data attibutes (d.data.id, etc.) need to 
-       match the column names in the models.py fiel exactly
-       and is case sensitive.
-       */
       .nodeContent(function (d, i, arr, state) {
-        const color = "#FFFFFF";
-        const imageDiffVert = 25 + 2;
-        return `
-            <div style='width:${
-        d.width
-            }px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
-              <div style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: ${d.data._highlighted || d.data._upToTheRootHighlighted ? '5px solid #FA3C3C"' : '1px solid #E4E2E9"'} >
-            <div style="color:#08011E;display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${
-              d.data.id
-            }</div>
-            <div style="background-color:${color};margin-top:${-imageDiffVert - 20}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
-            <div style="margin-top:${
-              -imageDiffVert - 20
-            }px;">   <img src=" ${d.data.image}" style="margin-left:${20}px;border-radius:100px;width:40px;height:40px;" /></div>
-            <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:10px">  ${
-              d.data.name
-            } </div>
-            <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
-              d.data.position
-            } </div>
-            <div class="node-salaries hidden" style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
-              new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(d.data.salary)
-            } </div>
-             <div style="display:flex;justify-content:space-between;padding-left:15px;padding-right:15px;">
-            <div style="color:#08011E; position: absolute; bottom: 10px; left: 15px;" class="node-direct-reports">Directs: ${
-              d.data._directSubordinates
-            } </div>  
-            <div style="color:#08011E; position: absolute; bottom: 10px; right: 15px;" class="node-total-reports">Total: ${
-              d.data._totalSubordinates
-            } </div>
-              </div>
-
-              </div>
-          </div>
-            `;
-      })
+        return genContent(d);
+    })
       .nodeUpdate(function (d) {
         // Needed to disable default highlight behavior
         d3.select(this).select(".node-rect").attr("stroke", "none");
