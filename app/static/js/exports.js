c8a345fa08b76083ec6a776172f16d9d5f39c545
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-07-04 16:31:13 -0400

    bugfix: removed export svg and data as they always included salaries which is a security issue (#19)

--- app/static/js/exports.js
+++ app/static/js/exports.js
@@ -1,31 +1,3 @@
-function exportSVG() {
-	setTimeout(function () {
-		chart.exportSvg();
-	}, 2000);
-}
-
 function exportPNG() {
 	chart.exportImg({ full: true });
-}
-
-function exportNodeData () {
-	const data = chart.data();
-	const nodeData = data.map(node => {
-		return {
-			id: node.id,
-			parentId: node.parentId,
-			name: node.name,
-			position: node.position,
-			salary: node.salary,
-			image: node.image
-		};
-	});
-
-	const blob = new Blob([JSON.stringify(nodeData, null, 2)], { type: 'application/json' });
-	const url = URL.createObjectURL(blob);
-	const a = document.createElement('a');
-	a.href = url;
-	a.download = 'chart-data.json';
-	a.click();
-	URL.revokeObjectURL(url);
 }
